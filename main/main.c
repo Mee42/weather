ca46c20571f7e9aa8366cbb0726fdf709f9471cb
Carson Graham <carson42g@gmail.com>
2022-12-14 16:07:08 -0500

    inital comit

--- main/main.c
+++ main/main.c
@@ -23,156 +23,151 @@
 
 #include <inttypes.h>
 
-#include "http.h"
-#include "wifi.h"
 #include "main.h"
 
-typedef struct {
-    gpio_num_t step;
-    gpio_num_t direction;
-    gpio_num_t enable;
-    gpio_num_t ms1;
-    gpio_num_t ms2;
-	int32_t   position;
-} stepper_t;
-
-stepper_t stepper_a = {
-    .step      = GPIO_NUM_15,
-    .direction = GPIO_NUM_2,
-    .enable    = GPIO_NUM_4,
-    .ms1       = GPIO_NUM_16,
-    .ms2       = GPIO_NUM_17,
-	.position  = 0
-};
-stepper_t stepper_b = {
-	.step      = GPIO_NUM_23,
-	.direction = GPIO_NUM_22,
-	.enable    = GPIO_NUM_21,
-	.ms1       = GPIO_NUM_19,
-	.ms2       = GPIO_NUM_18, 
-	.position  = 0
-};
 
 
-static void setup_gpio_output(gpio_num_t pin) {
-	// this is the new code, has not been tested yet
-	//gpio_reset_pin(pin);
-	//gpio_set_direction(pin, GPIO_MODE_OUTPUT);
-	//return; // in theory the above is the same as the below. TODO verify
-    gpio_config_t io_conf = {};
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    io_conf.pin_bit_mask = 1U << pin;
-    io_conf.pull_down_en = 0; // down-pull mode
-    io_conf.pull_up_en = 0; // pull-up mode
-    esp_err_t err = gpio_config(&io_conf);
-    if(err != ESP_OK) {
-        printf("Error while stetting pin %d as output pin\n", pin);
-    }
-}
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
 
-static void setup_stepper(stepper_t* stepper) {
-    setup_gpio_output(stepper->step);
-    setup_gpio_output(stepper->direction);
-    setup_gpio_output(stepper->enable);
-    setup_gpio_output(stepper->ms1);
-    setup_gpio_output(stepper->ms2);
-    gpio_set_level(stepper->direction, 0);
-    gpio_set_level(stepper->ms1, 1);
-    gpio_set_level(stepper->ms2, 1); // turn to 0 for big step
-    gpio_set_level(stepper->enable, 0);
-}
-
-void move_carriage_to(int x, int y) {
-	// TODO
-}
+#include "ssd1306.h"
+#include "font8x8_basic.h"
 
 
-// speed is in ticks per second
-void spin_stepper(int a_ticks, int b_ticks, int a_hz, int b_hz) {
-	gpio_set_level(stepper_a.direction, a_ticks < 0 ? 1 : 0);
-	gpio_set_level(stepper_b.direction, b_ticks < 0 ? 1 : 0);
-	a_ticks = a_ticks < 0 ? (0 - a_ticks) : a_ticks;
-	b_ticks = b_ticks < 0 ? (0 - b_ticks) : b_ticks;
+#define tag "SSD1306"
 
-	// we want to run two motors at variable, different ferquency
-	int period_a = 1000 * 1000  / a_hz / 2;
-	int period_b = 1000 * 1000  / b_hz / 2;
-	int64_t next_for_a = esp_timer_get_time() + period_a;
-	bool a_state = true;
-	int64_t next_for_b = esp_timer_get_time() + period_b;
-	bool b_state = true;
-	while(a_ticks > 0 || b_ticks > 0) {
-		// just a for now
-		int64_t current_time = esp_timer_get_time();
 
-		// we're going to see if 'a' or 'b' is closer to toggling	
-		// and then do that
-		if(next_for_a == next_for_b) {
-			next_for_a++; // if they overlap (likely if we keep our timings tight)
-			continue;     // then just do 'b', and do 'a' next microsecond
-						  // we don't need to be precice on timings here
-		} else if(next_for_a < next_for_b) {
-			int64_t delta = next_for_a - current_time;
-			if(delta >= 0) {
-				ets_delay_us(next_for_a - current_time);
-			}
-			// TODO do a more real time way of getting next_for_a 
-			// so we don't drift over time??
-			// might be better for lines and shit
-			next_for_a = esp_timer_get_time() + period_a;
 
-			if(a_ticks == 0) continue;
-			// we do this after all the other stuff
-			// so that it doesn't impact timings when a finishes
-			// I think that makes sense
+void app_main(void)
+{
+	SSD1306_t dev;
+	int center, top, bottom;
+	char lineChar[20];
 
-			a_state = !a_state;
-			gpio_set_level(stepper_a.step, a_state); 
-			if(a_state == false) {
-				// when the falling edge of the pulse, basically
-				// that's what we count as the end of the step
-				a_ticks--;
-			}
-		} else {
-			// b is closer to toggling
-			int64_t delta = next_for_b - current_time;
-			if(delta >= 0) {
-				ets_delay_us(next_for_b - current_time);
-			}
-			next_for_b = esp_timer_get_time() + period_b;
-		
-			if(b_ticks == 0) continue;
+	ESP_LOGI(tag, "INTERFACE is i2c");
+	ESP_LOGI(tag, "CONFIG_SDA_GPIO=%d",CONFIG_SDA_GPIO);
+	ESP_LOGI(tag, "CONFIG_SCL_GPIO=%d",CONFIG_SCL_GPIO);
+	ESP_LOGI(tag, "CONFIG_RESET_GPIO=%d",CONFIG_RESET_GPIO);
+	i2c_master_init(&dev, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
 
-			b_state = !b_state;
-			gpio_set_level(stepper_b.step, b_state);
-			if(b_state == false) {
-				b_ticks--;
-			}
-		}
+	ESP_LOGI(tag, "Panel is 128x64");
+	ssd1306_init(&dev, 128, 64);
+
+	ssd1306_clear_screen(&dev, true);
+	ssd1306_contrast(&dev, 0xff);
+  	ssd1306_display_text_x3(&dev, 1, "IT'S", 5, true);
+  	ssd1306_display_text_x3(&dev, 4, " COLD", 5, true);
+
+  	vTaskDelay(30000000000 / portTICK_PERIOD_MS);
+
+	top = 2;
+	center = 3;
+	bottom = 8;
+	ssd1306_display_text(&dev, 0, "SSD1306 128x64", 14, false);
+	ssd1306_display_text(&dev, 1, "ABCDEFGHIJKLMNOP", 16, false);
+	ssd1306_display_text(&dev, 2, "abcdefghijklmnop",16, false);
+	ssd1306_display_text(&dev, 3, "Hello World!!", 13, false);
+	//ssd1306_clear_line(&dev, 4, true);
+	//ssd1306_clear_line(&dev, 5, true);
+	//ssd1306_clear_line(&dev, 6, true);
+	//ssd1306_clear_line(&dev, 7, true);
+	ssd1306_display_text(&dev, 4, "SSD1306 128x64", 14, true);
+	ssd1306_display_text(&dev, 5, "ABCDEFGHIJKLMNOP", 16, true);
+	ssd1306_display_text(&dev, 6, "abcdefghijklmnop",16, true);
+	ssd1306_display_text(&dev, 7, "Hello World!!", 13, true);
+
+	vTaskDelay(3000 / portTICK_PERIOD_MS);
+	
+	// Display Count Down
+	uint8_t image[24];
+	memset(image, 0, sizeof(image));
+	ssd1306_display_image(&dev, top, (6*8-1), image, sizeof(image));
+	ssd1306_display_image(&dev, top+1, (6*8-1), image, sizeof(image));
+	ssd1306_display_image(&dev, top+2, (6*8-1), image, sizeof(image));
+	for(int font=0x39;font>0x30;font--) {
+		memset(image, 0, sizeof(image));
+		ssd1306_display_image(&dev, top+1, (7*8-1), image, 8);
+		memcpy(image, font8x8_basic_tr[font], 8);
+		if (dev._flip) ssd1306_flip(image, 8);
+		ssd1306_display_image(&dev, top+1, (7*8-1), image, 8);
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
-	printf("done spinning\n");
+	
+	// Scroll Up
+	ssd1306_clear_screen(&dev, false);
+	ssd1306_contrast(&dev, 0xff);
+	ssd1306_display_text(&dev, 0, "---Scroll  UP---", 16, true);
+	//ssd1306_software_scroll(&dev, 7, 1);
+	ssd1306_software_scroll(&dev, (dev._pages - 1), 1);
+	for (int line=0;line<bottom+10;line++) {
+		lineChar[0] = 0x01;
+		sprintf(&lineChar[1], " Line %02d", line);
+		ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
+		vTaskDelay(500 / portTICK_PERIOD_MS);
+	}
+	vTaskDelay(3000 / portTICK_PERIOD_MS);
+	
+	// Scroll Down
+	ssd1306_clear_screen(&dev, false);
+	ssd1306_contrast(&dev, 0xff);
+	ssd1306_display_text(&dev, 0, "--Scroll  DOWN--", 16, true);
+	//ssd1306_software_scroll(&dev, 1, 7);
+	ssd1306_software_scroll(&dev, 1, (dev._pages - 1) );
+	for (int line=0;line<bottom+10;line++) {
+		lineChar[0] = 0x02;
+		sprintf(&lineChar[1], " Line %02d", line);
+		ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
+		vTaskDelay(500 / portTICK_PERIOD_MS);
+	}
+	vTaskDelay(3000 / portTICK_PERIOD_MS);
+
+	// Page Down
+	ssd1306_clear_screen(&dev, false);
+	ssd1306_contrast(&dev, 0xff);
+	ssd1306_display_text(&dev, 0, "---Page	DOWN---", 16, true);
+	ssd1306_software_scroll(&dev, 1, (dev._pages-1) );
+	for (int line=0;line<bottom+10;line++) {
+		//if ( (line % 7) == 0) ssd1306_scroll_clear(&dev);
+		if ( (line % (dev._pages-1)) == 0) ssd1306_scroll_clear(&dev);
+		lineChar[0] = 0x02;
+		sprintf(&lineChar[1], " Line %02d", line);
+		ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
+		vTaskDelay(500 / portTICK_PERIOD_MS);
+	}
+	vTaskDelay(3000 / portTICK_PERIOD_MS);
+
+	// Horizontal Scroll
+	ssd1306_clear_screen(&dev, false);
+	ssd1306_contrast(&dev, 0xff);
+	ssd1306_display_text(&dev, center, "Horizontal", 10, false);
+	ssd1306_hardware_scroll(&dev, SCROLL_RIGHT);
+	vTaskDelay(5000 / portTICK_PERIOD_MS);
+	ssd1306_hardware_scroll(&dev, SCROLL_LEFT);
+	vTaskDelay(5000 / portTICK_PERIOD_MS);
+	ssd1306_hardware_scroll(&dev, SCROLL_STOP);
+	
+	// Vertical Scroll
+	ssd1306_clear_screen(&dev, false);
+	ssd1306_contrast(&dev, 0xff);
+	ssd1306_display_text(&dev, center, "Vertical", 8, false);
+	ssd1306_hardware_scroll(&dev, SCROLL_DOWN);
+	vTaskDelay(5000 / portTICK_PERIOD_MS);
+	ssd1306_hardware_scroll(&dev, SCROLL_UP);
+	vTaskDelay(5000 / portTICK_PERIOD_MS);
+	ssd1306_hardware_scroll(&dev, SCROLL_STOP);
+	
+	// Invert
+	ssd1306_clear_screen(&dev, true);
+	ssd1306_contrast(&dev, 0xff);
+	ssd1306_display_text(&dev, center, "  Good Bye!!", 12, true);
+	vTaskDelay(5000 / portTICK_PERIOD_MS);
+
+	// Fade Out
+	ssd1306_fadeout(&dev);
+	
+	// Restart module
+	esp_restart();
 }
-
-
-void app_main(void) { 
-    setup_stepper(&stepper_a);
-	setup_stepper(&stepper_b);
-
-    //Initialize NVS
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
-
-    
-    initalize_wifi();
-    start_webserver();
-}
-
-
-
 
 
--- main/main.h
+++ main/main.h
@@ -1,13 +1,1 @@
 #pragma once
-
-typedef enum {
-	DIRECTION_EXTRUDE, 
-	DIRECTION_INTAKE
-} direction_t;
-
-void spin_stepper(int a_ticks, int b_ticks, int a_hz, int b_hz);
-
-
-
-
-
